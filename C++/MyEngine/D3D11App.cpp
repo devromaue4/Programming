c6d2bead2ca2d673034b923707c48546b17147e6
devromaue4 <devroma@hotmail.com>
2014-12-12 23:04:28 +0600

    test 3d space

--- C++/MyEngine/D3D11App.cpp
+++ C++/MyEngine/D3D11App.cpp
@@ -6,9 +6,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
 
-// for vs2013
-#pragma warning(disable:4005)
-
+#pragma warning(disable:4005) // redefinition dx headers (for vs2013)
+#pragma warning(disable:4100) // unreference parameters
 
 #include <windows.h>
 #include <d3d11.h>
@@ -27,9 +26,10 @@ using namespace std;
 struct VERTEX
 {
 	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
 };
 
-struct ConstBuffer
+struct ConstantBuffer
 {
 	XMMATRIX mWorld;
 	XMMATRIX mView;
@@ -50,6 +50,11 @@ ID3D11VertexShader*     g_pVS;
 ID3D11PixelShader*      g_pPS;
 ID3D11InputLayout*      g_pVertLayout;
 ID3D11Buffer*           g_pVBuffer;
+ID3D11Buffer*           g_pIBuffer;
+ID3D11Buffer*           g_pConstBuffer;
+XMMATRIX                g_World;
+XMMATRIX                g_View;
+XMMATRIX                g_Projection;
 
 //--------------------------------------------------------------------------------------
 // Forward declarations
@@ -71,8 +76,10 @@ int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdL
 int main()
 #endif
 {
-    //UNREFERENCED_PARAMETER( hPrevInstance );
+    UNREFERENCED_PARAMETER( hPrevInstance );
     //UNREFERENCED_PARAMETER( lpCmdLine );
+	//UNREFERENCED_PARAMETER(nCmdShow);
+	//UNREFERENCED_PARAMETER(hInstance);
 
 	cout << "InitWindow" << endl;
 	if (FAILED(InitWindow(GetModuleHandle(NULL), SW_SHOWDEFAULT)))
@@ -123,7 +130,7 @@ HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow )
     wcex.hCursor = LoadCursor( NULL, IDC_ARROW );
     wcex.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1 );
     wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = L"TutorialWindowClass";
+    wcex.lpszClassName = L"MEWindowClass";
     wcex.hIconSm = LoadIcon( wcex.hInstance, ( LPCTSTR )IDI_TUTORIAL1 );
     if( !RegisterClassEx( &wcex ) )
         return E_FAIL;
@@ -132,7 +139,7 @@ HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow )
     g_hInst = hInstance;
     RECT rc = { 0, 0, 640, 480 };
     AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
-    g_hWnd = CreateWindow( L"TutorialWindowClass", L"Direct3D 11 Tutorial 6", WS_OVERLAPPEDWINDOW,
+    g_hWnd = CreateWindow( L"MEWindowClass", L"Direct3D11: Create 3D space", WS_OVERLAPPEDWINDOW,
                            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
                            NULL );
     if( !g_hWnd )
@@ -257,7 +264,8 @@ HRESULT InitDevice()
 
 	D3D11_INPUT_ELEMENT_DESC layout[] =
 	{
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 	};
 	UINT numElements = ARRAYSIZE(layout);
 	
@@ -284,9 +292,9 @@ HRESULT InitDevice()
 
 	VERTEX Vertices[] =
 	{
-		XMFLOAT3(0.0f, 0.5f, 0.5f),
-		XMFLOAT3(0.5f, -0.5f, 0.5f),
-		XMFLOAT3(-0.5f, -0.5f, 0.5f),
+		XMFLOAT3(-1.0f, 0.0f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f),
+		XMFLOAT3(0.0f, 1.0f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f),
+		XMFLOAT3(1.0f, 0.0f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f),
 	};
 
 	D3D11_BUFFER_DESC bd;
@@ -308,6 +316,27 @@ HRESULT InitDevice()
 	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &stride, &offset);
 
 	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Create the constant buffer
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(ConstantBuffer);
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bd.CPUAccessFlags = 0;
+	hr = g_pd3dDevice->CreateBuffer(&bd, NULL, &g_pConstBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Initialize the world matrix
+	g_World = XMMatrixIdentity();
+
+	// Initialize the view matrix
+	XMVECTOR Eye = XMVectorSet(0.0f, 0.0f, -2.0f, 0.0f);
+	XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	g_View = XMMatrixLookAtLH(Eye, At, Up);
+
+	// Initialize the projection matrix
+	g_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV2, width / (FLOAT)height, 0.01f, 100000.0f);
 
     return S_OK;
 }
@@ -359,11 +388,41 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
 //--------------------------------------------------------------------------------------
 void Render()
 {
+	// Update our time
+	static float t = 0.0f;
+	//if (g_driverType == D3D_DRIVER_TYPE_REFERENCE)
+	//{
+	//	t += (float)XM_PI * 0.0125f;
+	//}
+	//else
+	//{
+		static DWORD dwTimeStart = 0;
+		DWORD dwTimeCur = GetTickCount();
+		if (dwTimeStart == 0)
+			dwTimeStart = dwTimeCur;
+		t = (dwTimeCur - dwTimeStart) / 1000.0f;
+	//}
+
+	//
+	// Animate the cube
+	//
+	g_World = XMMatrixRotationY(t);
+
 	// Just clear the backbuffer
 	float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f }; //red,green,blue,alpha
 	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
 
+	//
+	// Update variables
+	//
+	ConstantBuffer cb;
+	cb.mWorld = XMMatrixTranspose(g_World);
+	cb.mView = XMMatrixTranspose(g_View);
+	cb.mProjection = XMMatrixTranspose(g_Projection);
+	g_pImmediateContext->UpdateSubresource(g_pConstBuffer, 0, NULL, &cb, 0, 0);
+
 	g_pImmediateContext->VSSetShader(g_pVS, NULL, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstBuffer);
 	g_pImmediateContext->PSSetShader(g_pPS, NULL, 0);
 	g_pImmediateContext->Draw(3, 0);
 
--- C++/MyEngine/Test.fx
+++ C++/MyEngine/Test.fx
@@ -4,19 +4,41 @@
 // Copyright (c) . All rights reserved.
 //--------------------------------------------------------------------------------------
 
+//--------------------------------------------------------------------------------------
+// Constant Buffer Variables
+//--------------------------------------------------------------------------------------
+cbuffer ConstantBuffer : register(b0)
+{
+	matrix World;
+	matrix View;
+	matrix Projection;
+}
+
+//--------------------------------------------------------------------------------------
+struct VS_OUTPUT
+{
+	float4 Pos : SV_POSITION;
+	float4 Color : COLOR0;
+};
+
 //
 // vs shader
 //
-float4 VS(float4 Pos : POSITION) : SV_POSITION
+VS_OUTPUT VS(float4 Pos : POSITION, float4 Color : COLOR)
 {
-	return Pos;
+	VS_OUTPUT Output = (VS_OUTPUT)0;
+	Output.Pos = mul(Pos, World);
+	Output.Pos = mul(Output.Pos, View);
+	Output.Pos = mul(Output.Pos, Projection);
+	Output.Color = Color;
+	return Output;
 }
 
 
 //
 // ps shader
 //
-float4 PS( float4 Pos : SV_POSITION ) : SV_Target
+float4 PS(VS_OUTPUT Input) : SV_Target
 {
-	return float4(1.0f, 1.0f, 0.0f, 1.0f); 
+	return Input.Color;
 }
