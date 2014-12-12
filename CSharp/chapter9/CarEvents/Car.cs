2ed0db4f1afb3d0c166102f0105fdf48aaa388be
devromaue4 <devroma@hotmail.com>
2014-12-12 15:52:59 +0600

    Events

---
+++ CSharp/chapter9/CarEvents/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public delegate void CarEngineHandler(string msgForCaller);
+        //private CarEngineHandler listOfHandlers;
+
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        // Функция которая позволяет вызывающему коду указыать метод для обратного вызова
+        //public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        //{
+        //    listOfHandlers += methodToCall;
+        //}
+        //public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        //{
+        //    listOfHandlers -= methodToCall;
+        //}
+
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSpeed, int currSpeed)
+        {
+            CurrentSpeed = currSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = name;
+        }
+
+        // Реализовать метод Accelerate() для обращения
+        // к списку вызовов делегата при нужных условиях
+        public void Accelerate(int delta)
+        {
+            //// Если автомобиль сломан, отправить сообщение об этом
+            //if (carIsDead)
+            //{
+            //    if (listOfHandlers != null)
+            //        listOfHandlers("Sorry, this car is dead...");
+            //}
+            //else
+            //{
+            //    CurrentSpeed += delta;
+            //    // Автомобиль почти сломан?
+            //    if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+            //    {
+            //        listOfHandlers("Careful buddy! Gonna blow!");
+            //    }
+
+            //    if (CurrentSpeed >= MaxSpeed)
+            //        carIsDead = true;
+            //    else
+            //        Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            //}
+
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded("Sorry, this car is dead...");
+            
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (10 == MaxSpeed - CurrentSpeed && AboutToBlow != null)
+                    AboutToBlow("Careful buddy! Gonna blow!");
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CSharp/chapter9/CarEvents/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Events *****\n");
+            Car car1 = new Car("SlugBug", 100, 10);
+            car1.AboutToBlow += CarIsAlmostDoomed;
+            car1.AboutToBlow += CarIsAboutToBlow;
+            car1.Exploded += CarExploded;
+
+            Console.WriteLine("***** Speed up *****");
+            for (int i = 0; i < 6; i++)
+                car1.Accelerate(20);
+
+            car1.Exploded -= CarExploded;
+
+            Console.WriteLine("\n***** Speed up *****");
+            for (int i = 0; i < 6; i++)
+                car1.Accelerate(20);
+
+            Console.ReadLine();
+
+        }
+
+
+        public static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void CarIsAboutToBlow(string msg)
+        {
+            Console.WriteLine("=> Critical Message from Car: {0}", msg);
+        }
+
+        public static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
