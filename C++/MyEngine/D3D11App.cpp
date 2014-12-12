cedb9e909b013e4a0a114a65265011f9369c081f
devromaue4 <devroma@hotmail.com>
2014-12-13 00:01:10 +0600

    add class RenderD3D11

--- C++/MyEngine/D3D11App.cpp
+++ C++/MyEngine/D3D11App.cpp
@@ -6,19 +6,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
 
-#pragma warning(disable:4005) // redefinition dx headers (for vs2013)
+
 #pragma warning(disable:4100) // unreference parameters
 
 #include <windows.h>
-#include <d3d11.h>
-#include <d3dx11.h>
-#include <d3dcompiler.h>
-#include <xnamath.h>
 #include "resource.h"
-
 #include <iostream>
 #include <vector>
 using namespace std;
+#include "RenderD3D11.h"
+
 
 //--------------------------------------------------------------------------------------
 // Structures
@@ -41,11 +38,6 @@ struct ConstantBuffer
 //--------------------------------------------------------------------------------------
 HINSTANCE               g_hInst = NULL;
 HWND                    g_hWnd = NULL;
-D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*           g_pd3dDevice = NULL;
-ID3D11DeviceContext*    g_pImmediateContext = NULL;
-IDXGISwapChain*         g_pSwapChain = NULL;
-ID3D11RenderTargetView* g_pRenderTargetView = NULL;
 ID3D11VertexShader*     g_pVS;
 ID3D11PixelShader*      g_pPS;
 ID3D11InputLayout*      g_pVertLayout;
@@ -186,66 +178,6 @@ HRESULT CompileShaderFromFile(WCHAR* sFileName, LPCSTR sEntryPoint, LPCSTR sShad
 HRESULT InitDevice()
 {
     HRESULT hr = S_OK;
-
-	RECT rc;
-	GetClientRect(g_hWnd, &rc);
-	UINT width = rc.right - rc.left;
-	UINT height = rc.bottom - rc.top;
-
-	UINT createDeviceFlags = 0;
-#ifdef _DEBUG
-	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-
-	D3D_FEATURE_LEVEL featureLevels[] =
-	{
-		D3D_FEATURE_LEVEL_11_0,
-		D3D_FEATURE_LEVEL_10_1,
-		D3D_FEATURE_LEVEL_10_0,
-	};
-	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-
-	DXGI_SWAP_CHAIN_DESC sd;
-	ZeroMemory(&sd, sizeof(sd));
-	sd.BufferCount = 1;
-	sd.BufferDesc.Width = width;
-	sd.BufferDesc.Height = height;
-	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 60;
-	sd.BufferDesc.RefreshRate.Denominator = 1;
-	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	sd.OutputWindow = g_hWnd;
-	sd.SampleDesc.Count = 1;
-	sd.SampleDesc.Quality = 0;
-	sd.Windowed = TRUE;
-
-	hr = D3D11CreateDeviceAndSwapChain( NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevels,
-		numFeatureLevels, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext );
-	if (FAILED(hr))
-		return hr;
-
-	// Create a render target view
-	ID3D11Texture2D* pBackBuffer = NULL;
-	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
-	if (FAILED(hr))
-		return hr;
-
-	hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_pRenderTargetView);
-	pBackBuffer->Release();
-	if (FAILED(hr))
-		return hr;
-
-	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, NULL);
-
-	// Setup the viewport
-	D3D11_VIEWPORT vp;
-	vp.Width = (FLOAT)width;
-	vp.Height = (FLOAT)height;
-	vp.MinDepth = 0.0f;
-	vp.MaxDepth = 1.0f;
-	vp.TopLeftX = 0;
-	vp.TopLeftY = 0;
-	g_pImmediateContext->RSSetViewports(1, &vp);
 	
 	ID3DBlob* pVSBlob = NULL;
 	hr = CompileShaderFromFile(L"Test.fx", "VS", "vs_4_0", &pVSBlob);
@@ -255,7 +187,13 @@ HRESULT InitDevice()
 		return hr;
 	}
 
-	hr = g_pd3dDevice->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pVS);
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+	GRenderD3D11->Init(g_hWnd, width, height);
+
+	hr = GRenderD3D11->GetDevice()->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pVS);
 	if( FAILED( hr ) )
 	{
 		pVSBlob->Release();
@@ -269,13 +207,13 @@ HRESULT InitDevice()
 	};
 	UINT numElements = ARRAYSIZE(layout);
 	
-	hr = g_pd3dDevice->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(),
+	hr = GRenderD3D11->GetDevice()->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(),
 		&g_pVertLayout);
 	pVSBlob->Release();
 	if (FAILED(hr))
 		return hr;
 
-	g_pImmediateContext->IASetInputLayout(g_pVertLayout);
+	GRenderD3D11->GetImmediateContext()->IASetInputLayout(g_pVertLayout);
 
 	//ID3DBlob* pPSBlob = NULL;
 	hr = CompileShaderFromFile(L"Test.fx", "PS", "ps_4_0", &pVSBlob);
@@ -285,7 +223,7 @@ HRESULT InitDevice()
 		return hr;
 	}
 
-	hr = g_pd3dDevice->CreatePixelShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pPS);
+	hr = GRenderD3D11->GetDevice()->CreatePixelShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pPS);
 	pVSBlob->Release();
 	if (FAILED(hr))
 		return hr;
@@ -307,22 +245,22 @@ HRESULT InitDevice()
 	ZeroMemory(&subData, sizeof(subData));
 	subData.pSysMem = Vertices;
 
-	hr = g_pd3dDevice->CreateBuffer(&bd, &subData, &g_pVBuffer);
+	hr = GRenderD3D11->GetDevice()->CreateBuffer(&bd, &subData, &g_pVBuffer);
 	if (FAILED(hr))
 		return hr;
 
 	UINT stride = sizeof(VERTEX);
 	UINT offset = 0;
-	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &stride, &offset);
+	GRenderD3D11->GetImmediateContext()->IASetVertexBuffers(0, 1, &g_pVBuffer, &stride, &offset);
 
-	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	GRenderD3D11->GetImmediateContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	// Create the constant buffer
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(ConstantBuffer);
 	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
 	bd.CPUAccessFlags = 0;
-	hr = g_pd3dDevice->CreateBuffer(&bd, NULL, &g_pConstBuffer);
+	hr = GRenderD3D11->GetDevice()->CreateBuffer(&bd, NULL, &g_pConstBuffer);
 	if (FAILED(hr))
 		return hr;
 
@@ -347,12 +285,7 @@ HRESULT InitDevice()
 //--------------------------------------------------------------------------------------
 void CleanupDevice()
 {
-	if (g_pImmediateContext) g_pImmediateContext->ClearState();
-
-	if (g_pRenderTargetView) g_pRenderTargetView->Release();
-	if (g_pSwapChain) g_pSwapChain->Release();
-	if (g_pImmediateContext) g_pImmediateContext->Release();
-	if (g_pd3dDevice) g_pd3dDevice->Release();
+	GRenderD3D11->Relese();
 }
 
 
@@ -390,18 +323,11 @@ void Render()
 {
 	// Update our time
 	static float t = 0.0f;
-	//if (g_driverType == D3D_DRIVER_TYPE_REFERENCE)
-	//{
-	//	t += (float)XM_PI * 0.0125f;
-	//}
-	//else
-	//{
-		static DWORD dwTimeStart = 0;
-		DWORD dwTimeCur = GetTickCount();
-		if (dwTimeStart == 0)
-			dwTimeStart = dwTimeCur;
-		t = (dwTimeCur - dwTimeStart) / 1000.0f;
-	//}
+	static DWORD dwTimeStart = 0;
+	DWORD dwTimeCur = GetTickCount();
+	if (dwTimeStart == 0)
+		dwTimeStart = dwTimeCur;
+	t = (dwTimeCur - dwTimeStart) / 1000.0f;
 
 	//
 	// Animate the cube
@@ -410,7 +336,8 @@ void Render()
 
 	// Just clear the backbuffer
 	float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f }; //red,green,blue,alpha
-	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+	ID3D11DeviceContext* pImmediateContext = GRenderD3D11->GetImmediateContext();
+	pImmediateContext->ClearRenderTargetView(GRenderD3D11->GetRenderTargetView(), ClearColor);
 
 	//
 	// Update variables
@@ -419,14 +346,14 @@ void Render()
 	cb.mWorld = XMMatrixTranspose(g_World);
 	cb.mView = XMMatrixTranspose(g_View);
 	cb.mProjection = XMMatrixTranspose(g_Projection);
-	g_pImmediateContext->UpdateSubresource(g_pConstBuffer, 0, NULL, &cb, 0, 0);
+	pImmediateContext->UpdateSubresource(g_pConstBuffer, 0, NULL, &cb, 0, 0);
 
-	g_pImmediateContext->VSSetShader(g_pVS, NULL, 0);
-	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstBuffer);
-	g_pImmediateContext->PSSetShader(g_pPS, NULL, 0);
-	g_pImmediateContext->Draw(3, 0);
+	pImmediateContext->VSSetShader(g_pVS, NULL, 0);
+	pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstBuffer);
+	pImmediateContext->PSSetShader(g_pPS, NULL, 0);
+	pImmediateContext->Draw(3, 0);
 
-	g_pSwapChain->Present(0, 0);
+	GRenderD3D11->GetSwapChain()->Present(0, 0);
 }
 
 
---
+++ C++/MyEngine/RenderD3D11.h
@@ -0,0 +1,32 @@
+#pragma once
+#pragma warning(disable:4005) // redefinition dx headers (for vs2013)
+
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include <xnamath.h>
+
+
+class RenderD3D11
+{
+	D3D_FEATURE_LEVEL       m_featureLevel;
+	ID3D11Device*           m_pd3dDevice;
+	ID3D11DeviceContext*    m_pImmediateContext;
+	IDXGISwapChain*         m_pSwapChain;
+	ID3D11RenderTargetView* m_pRenderTargetView;
+
+public:
+	RenderD3D11();
+	~RenderD3D11();
+
+	bool Init(HWND hWnd, UINT Width, UINT Height);
+	void Relese();
+
+	ID3D11Device*           GetDevice()           { return m_pd3dDevice; }
+	ID3D11DeviceContext*    GetImmediateContext() { return m_pImmediateContext; }
+	IDXGISwapChain*         GetSwapChain()        { return m_pSwapChain; }
+	ID3D11RenderTargetView* GetRenderTargetView() { return m_pRenderTargetView; }
+};
+
+
+extern RenderD3D11* GRenderD3D11;
