6f2fba635f9d4a22aa131400f9a0641a50e17fb7
devromaue4 <devroma@hotmail.com>
2015-01-07 14:03:56 +0600

    no comm

---
+++ CSharp/chapter9/GenericPrimAndProperCarEvents/Car.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrimAndProperCarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        //public delegate void CarEngineHandler(string msgForCaller);
+        //private CarEngineHandler listOfHandlers;
+        //public delegate void CarEngineHandler(object sender, CarEventArgs e);
+
+        //public event CarEngineHandler Exploded;
+        //public event CarEngineHandler AboutToBlow;
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> AboutToBlow;
+
+        // Функция которая позволяет вызывающему коду указыать метод для обратного вызова
+        //public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        //{
+        //    listOfHandlers += methodToCall;
+        //}
+        //public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        //{
+        //    listOfHandlers -= methodToCall;
+        //}
+
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSpeed, int currSpeed)
+        {
+            CurrentSpeed = currSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = name;
+        }
+
+        // Реализовать метод Accelerate() для обращения
+        // к списку вызовов делегата при нужных условиях
+        public void Accelerate(int delta)
+        {
+            //// Если автомобиль сломан, отправить сообщение об этом
+            //if (carIsDead)
+            //{
+            //    if (listOfHandlers != null)
+            //        listOfHandlers("Sorry, this car is dead...");
+            //}
+            //else
+            //{
+            //    CurrentSpeed += delta;
+            //    // Автомобиль почти сломан?
+            //    if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+            //    {
+            //        listOfHandlers("Careful buddy! Gonna blow!");
+            //    }
+
+            //    if (CurrentSpeed >= MaxSpeed)
+            //        carIsDead = true;
+            //    else
+            //        Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            //}
+
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded(this, new CarEventArgs("Sorry, this car is dead..."));
+            
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (10 == MaxSpeed - CurrentSpeed && AboutToBlow != null)
+                    AboutToBlow(this, new CarEventArgs("Careful buddy! Gonna blow!"));
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CSharp/chapter9/GenericPrimAndProperCarEvents/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrimAndProperCarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Events *****\n");
+
+            Car car = new Car("SlugBug", 100, 10);
+
+            // Register event handlers.
+            car.AboutToBlow += CarAboutToBlow;
+            //car.Exploded += CarExploded;
+
+            EventHandler<CarEventArgs> d = new EventHandler<CarEventArgs>(CarExploded);
+            car.Exploded += d;
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                car.Accelerate(20);
+
+            car.Exploded -= CarExploded;
+
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                car.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        //public static CarIsAlmostDoomed(object sender, CarEventArgs e)
+        //{
+        //    Console.WriteLine("=> Critical Mesage from Car: {0}", e.msg);
+        //}
+
+        public static void CarAboutToBlow(object sender, CarEventArgs e)
+        {
+            //Console.WriteLine("{0} says: {1}", sender, e.msg);
+
+            if(sender is Car)
+            {
+                Car c = (Car)sender;
+                Console.WriteLine("Critical Message from {0}: {1}", c.PetName, e.msg);
+            }
+        }
+
+        public static void CarExploded(object sender, CarEventArgs e)
+        {
+            Console.WriteLine(e.msg);
+        }
+    }
+}
